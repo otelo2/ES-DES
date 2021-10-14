3a0c69614dd28a7b06f5c09caa13c394ec91a8df
Otelo2 <josean2009@live.com.mx>
2021-10-14 13:30:37 -0500

    Working key generation

--- S-DES/SDES.py
+++ S-DES/SDES.py
@@ -1,8 +1,46 @@
-def define_inputs():
-    # 8 bit input
-    plaintext = "10111101"
-    # 10 bit input
-    key ="0000000000"
+# Performs a circular left shift of one (1) position
+def circular_left_shift_1(key):
+    # Divide input into 5-bit pairs
+    result_key = ""
+    left_shift_guide = [1,2,3,4,0]
+    for position in left_shift_guide:
+        result_key += key[position]
+    print(f"Output of circular left shift 1: {result_key}")
+    return result_key
+
+# Performs a circular left shift of two (2) positions
+def circular_left_shift_2(key):
+    # Divide input into 5-bit pairs
+    result_key = ""
+    left_shift_guide = [2,3,4,0,1]
+    for position in left_shift_guide:
+        result_key += key[position]
+    print(f"Output of circular left shift 2: {result_key}")
+    return result_key
+
+# Permutation 10.
+def P10(key):
+    # Output
+    permutated_key = ""
+    # Permutation table = 3,5,2,7,4,10,1,9,8,6
+    permutation_table = [3,5,2,7,4,10,1,9,8,6]
+    for position in permutation_table:
+        permutated_key += key[position-1]
+
+    print(f"P10 output: {permutated_key}")
+    return permutated_key
+
+# Picks out and permutes 8 og the 10 bits
+def P8(key):
+    # Output
+    permutated_key = ""
+    # Permutation table = 6,3,7,4,8,5,10,9
+    permutation_table = [6,3,7,4,8,5,10,9]
+    for position in permutation_table:
+        permutated_key += key[position-1]
+
+    print(f"P8 output: {permutated_key}")
+    return permutated_key
 
 # Generate two 8-bit subkeys from the 10-bit input
 def key_generation(key):
@@ -26,49 +64,6 @@ def key_generation(key):
     joined_key = double_left_shifted_left_key + double_left_shifted_right_key
     k2 = P8(joined_key)
     print(f"Subkey 2: {k2}")
-
-# Permutation 10.
-def P10(key):
-    # Output
-    permutated_key = ""
-    # Permutation table = 3,5,2,7,4,10,1,9,8,6
-    permutation_table = [3,5,2,7,4,10,1,9,8,6]
-    for position in permutation_table:
-        permutated_key += key[position-1]
-
-    print(f"P10 output: {permutated_key}")
-    return permutated_key
-    
-def circular_left_shift_1(key):
-    # Divide input into 5-bit pairs
-    result_key = ""
-    left_shift_guide = [1,2,3,4,0]
-    for position in left_shift_guide:
-        result_key += key[position]
-    print(f"Output of circular left shift 1: {result_key}")
-    return result_key
-
-def circular_left_shift_2(key):
-    # Divide input into 5-bit pairs
-    result_key = ""
-    left_shift_guide = [2,3,4,0,1]
-    for position in left_shift_guide:
-        result_key += key[position]
-    print(f"Output of circular left shift 2: {result_key}")
-    return result_key
-
-# Picks out and permutes 8 og the 10 bits
-def P8(key):
-    # Output
-    permutated_key = ""
-    # Permutation table = 6,3,7,4,8,5,10,9
-    permutation_table = [6,3,7,4,8,5,10,9]
-    for position in permutation_table:
-        permutated_key += key[position-1]
-
-    print(f"P8 output: {permutated_key}")
-    return permutated_key
-    
 
 def main():
     key = "1010000010"
