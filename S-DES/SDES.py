05bb6219851e120c5b3b3b6b2740dde45021af65
Otelo2 <josean2009@live.com.mx>
2021-10-14 14:24:37 -0500

    Up until the application of S-boxes

--- S-DES/SDES.py
+++ S-DES/SDES.py
@@ -64,10 +64,86 @@ def key_generation(key):
     joined_key = double_left_shifted_left_key + double_left_shifted_right_key
     k2 = P8(joined_key)
     print(f"Subkey 2: {k2}")
+    
+    return (k1, k2)
+
+#Initial permutation
+def IP(plaintext):
+    # Output
+    permutated_plaintext = ""
+    # Permutation table = 2,6,3,1,4,8,5,7
+    permutation_table = [2,6,3,1,4,8,5,7]
+    for position in permutation_table:
+        permutated_plaintext += plaintext[position-1]
+
+    print(f"IP output: {permutated_plaintext}")
+    return permutated_plaintext
+
+# Inverse initial permutation
+def IP_1(plaintext):
+    # Output
+    permutated_plaintext = ""
+    # Permutation table = 4,1,3,5,7,2,8,6
+    permutation_table = [4,1,3,5,7,2,8,6]
+    for position in permutation_table:
+        permutated_plaintext += plaintext[position-1]
+
+    print(f"Inverse IP output: {permutated_plaintext}")
+    return permutated_plaintext
+
+# Combination of permutation and substitution functions
+def f_k(left_input, right_input, subkey):
+    a=1
+    
+# Mapping from 4-bit strings to 4-bit strings
+# Input is a 4-bit number
+def F(plaintext, subkey):
+    # The first operation is an expansion/permutation operation
+    # Output
+    permutated_plaintext = ""
+    # Permutation table = 4,1,2,3,2,3,4,1
+    expansion_permutation_table = [4,1,2,3,2,3,4,1]
+    for position in expansion_permutation_table:
+        permutated_plaintext += plaintext[position-1]
+
+    print(f"F function output: {permutated_plaintext}")
+    
+    # The 8-bit subkey k1 is added to this value using XOR
+    P_string = ""
+    for index, plaintext_bit in enumerate(permutated_plaintext):
+        P_string += XOR(plaintext_bit, subkey[index])
+        
+    print(f"P string value: {P_string}")
+    
+    # The first 4 bits (first row of the P_string matrix)
+    # are fed into the S-box s0 to produce a 2-bit output
+    # The remaining 4 bits (second row) are def into S1 to produce another 2-bit output
+
+def XOR(plaintext_bit, subkey_bit):
+    # ^ is the bitwise XOR in python
+    n = int(plaintext_bit)
+    k = int(subkey_bit)
+    #print(f"{n} XOR {k} = {n^k}")
+    return str(n^k)
+
+def encryption(plaintext, key):
+    k1, k2 = key
+    initial_permutation = IP(plaintext)
+    left_input = initial_permutation[0:4]
+    right_input = initial_permutation[4:8]
+    
+    F(right_input, k1)
 
 def main():
+    # 10-bit key
     key = "1010000010"
-    key_generation(key)
+    # 8-bit plaintext
+    plaintext = "10010110"
+    # Generate subkeys from the key
+    key = key_generation(key)
+    
+    #Encrypt the plaintext using the key
+    encryption(plaintext, key)
 
 if __name__ == "__main__":
     main()
